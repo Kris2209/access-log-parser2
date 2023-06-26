cb19d704e6a871ba9b198327855da24f1ff986b1
Kristina <KYAkovenko@inno.tech>
2023-06-26 21:08:36 +0300

    Задание 2, сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,9 @@ public class Main {
         int count = 0;
         int number_doc = 0;
         int stringSum = 0;
-        int lenghtMax = 0;
-        int lenghtMin = 1024;
+        int googleBotCount = 0;
+        int yandexBotCount = 0;
+
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
@@ -28,17 +29,44 @@ public class Main {
                     while ((line = reader.readLine()) != null) {
                         int length = line.length();
                         if(length>1024) throw new ExceededTheLimit("Количество символов в строке не должно превышать 1024");
-                        if(length>lenghtMax) lenghtMax = length;
-                        if(length<lenghtMin) lenghtMin = length;
                         stringSum+=1;
-                    }
-                }
+
+                    char bracket = '"';
+                    int[] bracketIndices = new int[6];
+                    bracketIndices[0] = line.indexOf(bracket);
+                    bracketIndices[1] = line.indexOf(bracket, bracketIndices[0] + 1);
+                    bracketIndices[2] = line.indexOf(bracket, bracketIndices[1] + 1);
+                    bracketIndices[3] = line.indexOf(bracket, bracketIndices[2] + 1);
+                    bracketIndices[4] = line.indexOf(bracket, bracketIndices[3] + 1);
+                    bracketIndices[5] = line.indexOf(bracket, bracketIndices[4] + 1);
+
+                    String thirdBrackets = line.substring(bracketIndices[4], bracketIndices[5]);
+
+                    String[] parts = thirdBrackets.split(";");
+                    for (int i = 0; i < parts.length; i++) {
+                        parts[i] = parts[i].trim();}
+
+                    if (parts.length >= 2) {
+                        String fragment = parts[1];
+
+                        int slashIndex = fragment.indexOf('/');
+                        if (slashIndex > 0) {
+                            String bot = fragment.substring(0, slashIndex);
+                            if (bot.equalsIgnoreCase("Googlebot")) {
+                                googleBotCount++;
+                            } else if (bot.equalsIgnoreCase("YandexBot")) {
+                                yandexBotCount++;
+                            }
+                        }}
+
+                }}
                 catch(Exception ex){
                     ex.printStackTrace();
                 }
                 System.out.println("Количество строк: " + stringSum);
-                System.out.println("Длина самой длинной строки: " + lenghtMax);
-                System.out.println("Длина самой короткой строки: " + lenghtMin);
+                System.out.println("Запросов от Google: " + googleBotCount + " (" + ((double)googleBotCount / (double)stringSum * 100.0) + "%)");
+                System.out.println("Запросов от Yandex: " + yandexBotCount + " (" + (double)yandexBotCount / (double)stringSum * 100.0 + "%)");
+
                 continue;}
 
             if (fileExists && isDirectory) {
