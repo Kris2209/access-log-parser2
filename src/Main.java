b933b85954424c612cc90fae120fd33e05acc70a
Kristina <KYAkovenko@inno.tech>
2023-07-04 11:34:08 +0300

    Добавлены классы LogEntry, UserAgent, Statistics

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,51 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        String path;
+        Statistics statistics = new Statistics();
         int count = 0;
-        int number_doc = 0;
-        int stringSum = 0;
-        int googleBotCount = 0;
-        int yandexBotCount = 0;
-
         while (true) {
-            String path = new Scanner(System.in).nextLine();
+            System.out.println("Введите путь к файлу и нажмите <Enter>:");
+            path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            boolean fileExists = file.exists();
-            boolean isDirectory = file.isDirectory();
 
-            if (fileExists && !isDirectory){
-                number_doc +=1;
-                System.out.println("Путь указан верно");
-                System.out.println("Это файл номер " + number_doc);
+            boolean pathOfFile = file.exists();
+            boolean isNotDirectory = !file.isDirectory();
+            if (pathOfFile && isNotDirectory) {
+                System.out.println("Путь к файлу является правильным");
+                System.out.println("Это файл №" + ++count);
                 try {
                     FileReader fileReader = new FileReader(path);
                     BufferedReader reader =
                             new BufferedReader(fileReader);
+
                     String line;
                     while ((line = reader.readLine()) != null) {
                         int length = line.length();
-                        if(length>1024) throw new ExceededTheLimit("Количество символов в строке не должно превышать 1024");
-                        stringSum+=1;
 
-                    char bracket = '"';
-                    int[] bracketIndices = new int[6];
-                    bracketIndices[0] = line.indexOf(bracket);
-                    bracketIndices[1] = line.indexOf(bracket, bracketIndices[0] + 1);
-                    bracketIndices[2] = line.indexOf(bracket, bracketIndices[1] + 1);
-                    bracketIndices[3] = line.indexOf(bracket, bracketIndices[2] + 1);
-                    bracketIndices[4] = line.indexOf(bracket, bracketIndices[3] + 1);
-                    bracketIndices[5] = line.indexOf(bracket, bracketIndices[4] + 1);
+                        if (length > 1024) {
+                            throw new RuntimeException("Length of line > 1024");
+                        }
+                        LogEntry e = new LogEntry(line);
+                        statistics.addEntry(e);
 
-                    String thirdBrackets = line.substring(bracketIndices[4], bracketIndices[5]);
+                    }
 
-                    String[] parts = thirdBrackets.split(";");
-                    for (int i = 0; i < parts.length; i++) {
-                        parts[i] = parts[i].trim();}
-
-                    if (parts.length >= 2) {
-                        String fragment = parts[1];
-
-                        int slashIndex = fragment.indexOf('/');
-                        if (slashIndex > 0) {
-                            String bot = fragment.substring(0, slashIndex);
-                            if (bot.equalsIgnoreCase("Googlebot")) {
-                                googleBotCount++;
-                            } else if (bot.equalsIgnoreCase("YandexBot")) {
-                                yandexBotCount++;
-                            }
-                        }}
-
-                }}
-                catch(Exception ex){
-                    ex.printStackTrace();
+                    System.out.println("Общий трафик: " + statistics.totalTraffic + " bytes");
+                    System.out.println("Минимальное время: " + statistics.minTime);
+                    System.out.println("Максимальное время: " + statistics.maxTime);
+                    System.out.println("Скорость трафика: " + statistics.getTrafficRate() + " bytes per hour");
+                } catch (IOException e) {
+                    System.out.println(e.fillInStackTrace());
                 }
-                System.out.println("Количество строк: " + stringSum);
-                System.out.println("Запросов от Google: " + googleBotCount + " (" + ((double)googleBotCount / (double)stringSum * 100.0) + "%)");
-                System.out.println("Запросов от Yandex: " + yandexBotCount + " (" + (double)yandexBotCount / (double)stringSum * 100.0 + "%)");
 
-                continue;}
 
-            if (fileExists && isDirectory) {
-                System.out.println("Указанный путь является путем к папке, а не к файлу, попробуйте снова: ");
             }
-
-            else  {
-                System.out.println("Файла не существует, попробуйте снова: ");
-                }
-
-
-    }
-}
-}
+            else {
+                System.out.println("Файл отсутствует или путь ведет к директории файла");
+            }
+        }
+    }}
