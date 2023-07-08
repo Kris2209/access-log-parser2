a8acc8f3764950cef20bf800087b9d915e88506d
Kristina <KYAkovenko@inno.tech>
2023-07-08 09:41:03 +0300

    Добавлен расчет доли ОС

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -7,7 +8,6 @@ enum HTTPRequestMethod {
     GET,
     POST
 }
-
 public class LogEntry {
     final String IPAddress;
     final LocalDateTime date;
@@ -17,26 +17,24 @@ public class LogEntry {
     final String answerCode;
     final int answerSize;
     final String referer;
-
     public LogEntry(String string) {
         int startIndex, endIndex;
-
-
         endIndex = string.indexOf('-');
         IPAddress = string.substring(0, endIndex).trim();
         System.out.println("IP: " + IPAddress);
 
         startIndex = string.indexOf('[') + 1;
         endIndex = string.indexOf(']');
-        date = LocalDateTime.parse(string.substring(startIndex, endIndex), DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z").withLocale(Locale.US));
-        System.out.println("Дата: " + date);
+        //date = LocalDateTime.parse(string.substring(startIndex, endIndex), DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z"));
+        var formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
+        date = ZonedDateTime.parse(string.substring(startIndex, endIndex), formatter).toLocalDateTime();
+        System.out.println("date: " + date);
 
         startIndex = string.indexOf('"');
         endIndex = string.indexOf('"', startIndex + 1);
         int savedIndex = endIndex;
         String request = string.substring(startIndex + 1, endIndex);
         System.out.println(request);
-
         endIndex = request.indexOf(' ');
         String method = request.substring(0, endIndex);
         if (method.equalsIgnoreCase("GET")) {
@@ -46,22 +44,19 @@ public class LogEntry {
         } else {
             this.method = HTTPRequestMethod.Unknown;
         }
-        System.out.println("Метод: " + method);
+        System.out.println("method: " + method);
         startIndex = endIndex;
         endIndex = request.indexOf(' ', endIndex + 1);
         path = request.substring(startIndex, endIndex);
         System.out.println("path: " + path);
-
         startIndex = string.indexOf(' ', savedIndex + 1);
         endIndex = string.indexOf(' ', startIndex + 1);
         answerCode = string.substring(startIndex, endIndex).trim();
         System.out.println("answerCode: " + answerCode);
-
         startIndex = endIndex + 1;
         endIndex = string.indexOf(' ', startIndex + 1);
         answerSize = Integer.parseInt(string.substring(startIndex, endIndex));
         System.out.println("answerSize: " + answerSize);
-
         startIndex = string.indexOf('"', endIndex + 1) + 1;
         endIndex = string.indexOf('"', startIndex + 1);
         String referer = string.substring(startIndex, endIndex);
@@ -71,7 +66,6 @@ public class LogEntry {
             this.referer = referer;
         }
         System.out.println("referer: " + this.referer);
-
         startIndex = string.indexOf('"', endIndex + 1);
         endIndex = string.indexOf('"', startIndex + 1);
         userAgent = new UserAgent(string.substring(startIndex + 1, endIndex));
--- src/Statistics.java
+++ src/Statistics.java
@@ -1,9 +1,15 @@
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class Statistics {
     LogEntry[] entries;
     int lastEntryIndex = 0;
+    final HashSet<String> okPages;
+    final HashMap<String, Integer> osStat;
 
     long totalTraffic = 0;
     LocalDateTime minTime = LocalDateTime.MAX;
@@ -11,6 +17,8 @@ public class Statistics {
 
     Statistics() {
         entries = new LogEntry[200000];
+        okPages = new HashSet<>();
+        osStat = new HashMap<>();
     }
 
     public void addEntry(LogEntry entry) {
@@ -25,10 +33,29 @@ public class Statistics {
         if (entry.date.isAfter(maxTime)) {
             maxTime = entry.date;
         }
+        if (entry.answerCode.equals("200")) {
+            okPages.add(entry.path);
+        }
+
+        String os = entry.userAgent.operatingSystem;
+        osStat.put(os, osStat.getOrDefault(os, 0) + 1);
     }
 
     public int getTrafficRate() {
         long hours = ChronoUnit.HOURS.between(minTime, maxTime);
+        if (hours == 0)
+            hours = 1;
         return (int)(totalTraffic / hours);
+    }
+    public List<String> getOkPages() {
+        return Arrays.asList(okPages.toArray(new String[0]));
+    }
+
+    public HashMap<String, Double> getPlatformStatistics() {
+        HashMap<String, Double> result = new HashMap<>();
+        for (var entry : osStat.entrySet()) {
+            result.put(entry.getKey(), Double.valueOf(entry.getValue()) / osStat.size());
+        }
+        return result;
     }
 }
